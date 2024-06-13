15aa63e50f8495d3bd0b89069adf2f82b01c0adf
lukalomidze <lomidzeluka.w@gmail.com>
2024-06-13 20:50:52 +0400

    Rename dto

--- src/main/java/pt/ipcbcampus/lukalomidze/forum/dto/CreatePostDTO.java
+++ src/main/java/pt/ipcbcampus/lukalomidze/forum/dto/CreatePostDTO.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class PostDTO {
+public class CreatePostDTO {
     @NotBlank
     private String author;
     @NotBlank
